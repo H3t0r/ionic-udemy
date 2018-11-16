b6bad1db75a2bf8781cd2660cdfcdb214855d4af
H3t0r <isc.quch@gmail.com>
2018-11-15 22:05:50 -0600

    Add Done and Todo components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,34 +7,17 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { TodoComponent } from '../pages/todo/todo.component';
+import { DoneComponent } from '../pages/done/done.component';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 @NgModule({
-  declarations: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
+  declarations: [MyApp, AboutPage, ContactPage, HomePage, TabsPage, TodoComponent, DoneComponent],
+  imports: [BrowserModule, IonicModule.forRoot(MyApp)],
   bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+  entryComponents: [MyApp, AboutPage, ContactPage, HomePage, TabsPage],
+  providers: [StatusBar, SplashScreen, { provide: ErrorHandler, useClass: IonicErrorHandler }],
 })
 export class AppModule {}
