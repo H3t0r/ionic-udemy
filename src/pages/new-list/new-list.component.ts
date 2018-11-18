f344ef17bd751725df1e2ab340278564b5b98518
H3t0r <isc.quch@gmail.com>
2018-11-17 18:32:13 -0600

    Add AlretController as a previous step to creating a new list

--- src/pages/new-list/new-list.component.ts
+++ src/pages/new-list/new-list.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { NavParams } from 'ionic-angular';
+
+import { List } from '../../models';
 
 @Component({
   selector: 'app-new-list',
   templateUrl: 'new-list.component.html',
 })
 export class NewListPage {
-  constructor() {}
+  list: List;
+
+  constructor(private ionParams: NavParams) {
+    const title = this.ionParams.get('listName');
+    this.list = new List(title);
+  }
 }
--- src/pages/todo/todo.component.ts
+++ src/pages/todo/todo.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { TasksService } from '../../services/tasks.service';
 import { List } from '../../models';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 import { NewListPage } from '../new-list/new-list.component';
 
 @Component({
@@ -9,13 +9,43 @@ import { NewListPage } from '../new-list/new-list.component';
   templateUrl: 'todo.component.html',
 })
 export class TodoPage {
-  constructor(public tasks: TasksService, private nav: NavController) {}
+  constructor(
+    public tasks: TasksService,
+    private ionNav: NavController,
+    private ionAlert: AlertController
+  ) {}
 
   listSelected(list: List) {
     console.log(list);
   }
 
   addNewList() {
-    this.nav.push(NewListPage);
+    const newListAlert = this.ionAlert.create({
+      title: 'Create new list',
+      message: 'Give a name to your list below.',
+      inputs: [
+        {
+          name: 'listName',
+          placeholder: 'List name...',
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+        },
+        {
+          text: 'Add',
+          handler: input => {
+            if (input.listName.trim() === '') {
+              return;
+            }
+
+            this.ionNav.push(NewListPage, { listName: input.listName });
+          },
+        },
+      ],
+    });
+
+    newListAlert.present();
   }
 }
