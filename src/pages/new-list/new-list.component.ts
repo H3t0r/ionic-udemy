5d74f961f6b0feb115bef45e46d86a34d4e8d20d
H3t0r <isc.quch@gmail.com>
2018-11-17 19:53:45 -0600

    Add item bindings and add/remove/update logic

--- src/pages/new-list/new-list.component.ts
+++ src/pages/new-list/new-list.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NavParams } from 'ionic-angular';
 
-import { List } from '../../models';
+import { List, Item } from '../../models';
 
 @Component({
   selector: 'app-new-list',
@@ -9,9 +9,29 @@ import { List } from '../../models';
 })
 export class NewListPage {
   list: List;
+  item: Item;
+  itemName: string;
 
   constructor(private ionParams: NavParams) {
     const title = this.ionParams.get('listName');
     this.list = new List(title);
+  }
+
+  addItem() {
+    if (this.itemName.trim() === '') {
+      return;
+    }
+
+    this.item = new Item(this.itemName);
+    this.list.items.push(this.item);
+    this.itemName = '';
+  }
+
+  updateItem(item: Item) {
+    item.isChecked = !item.isChecked;
+  }
+
+  removeItem(i: number) {
+    this.list.items.splice(i, 1);
   }
 }
