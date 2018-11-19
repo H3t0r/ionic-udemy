72dd84c7448c251e02977a82dd789d74359250c7
H3t0r <isc.quch@gmail.com>
2018-11-19 08:54:14 -0600

    Delete extra code inside done component

--- src/pages/done/done.component.ts
+++ src/pages/done/done.component.ts
@@ -1,15 +1,9 @@
 import { Component } from '@angular/core';
-import { TasksService } from '../../services/tasks.service';
-import { List } from '../../models';
 
 @Component({
   selector: 'app-done',
   templateUrl: 'done.component.html',
 })
 export class DonePage {
-  constructor(public tasks: TasksService) {}
-
-  listSelected(list: List) {
-    console.log(list);
-  }
+  constructor() {}
 }
