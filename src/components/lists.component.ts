ed5522bb1eec19e9234cf56612becee44e63c6a6
H3t0r <isc.quch@gmail.com>
2018-11-18 23:17:31 -0600

    Add rename list functionality

--- src/components/lists.component.ts
+++ src/components/lists.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController, ItemSliding } from 'ionic-angular';
 import { List } from '../models';
 import { TasksService } from '../services/tasks.service';
 import { NewListPage } from '../pages/new-list/new-list.component';
@@ -10,7 +10,12 @@ import { NewListPage } from '../pages/new-list/new-list.component';
 })
 export class ListsComponent {
   @Input() isFinished: boolean;
-  constructor(public tasks: TasksService, private ionNav: NavController) {}
+
+  constructor(
+    public tasks: TasksService,
+    private ionNav: NavController,
+    private ionAlert: AlertController
+  ) {}
 
   listSelected(list: List) {
     this.ionNav.push(NewListPage, { listName: list.title, list });
@@ -18,5 +23,38 @@ export class ListsComponent {
 
   removeList(id: number) {
     this.tasks.removeList(id);
+  }
+
+  renameList(list: List, item: ItemSliding) {
+    item.close();
+    const newListAlert = this.ionAlert.create({
+      title: 'Rename list',
+      message: 'Change the name of the list below',
+      inputs: [
+        {
+          name: 'listName',
+          placeholder: 'List name...',
+          value: list.title,
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+        },
+        {
+          text: 'Update',
+          handler: input => {
+            if (input.listName.trim() === '') {
+              return;
+            }
+
+            list.title = input.listName;
+            this.tasks.saveToStorage();
+          },
+        },
+      ],
+    });
+
+    newListAlert.present();
   }
 }
